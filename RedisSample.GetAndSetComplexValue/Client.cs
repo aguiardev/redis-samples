26aa24bdb59a273c6a2b393b968097df3461dc5b
Tiago Aguiar <tiago.aguiar1@hotmail.com>
2021-08-02 22:47:13 -0300

    fix template string and format date

--- RedisSample.GetAndSetComplexValue/Client.cs
+++ RedisSample.GetAndSetComplexValue/Client.cs
@@ -4,12 +4,12 @@ namespace RedisSample.GetAndSetComplexValue
 {
     public class Client
     {
-        private const string template = "CPF: {0} - Name: {1} - Document: {2}";
+        private const string template = "CPF: {0} - Name: {1} - Birth: {2}";
         public string Cpf { get; private set;}
         public string Name { get; set; }
         public DateTime Birth { get; set; }
         public Client(string cpf) => Cpf = cpf;
 
-        public override string ToString() => string.Format(template, Cpf, Name, Birth);
+        public override string ToString() => string.Format(template, Cpf, Name, Birth.ToString("dd/MM/yyyy"));
     }
 }
